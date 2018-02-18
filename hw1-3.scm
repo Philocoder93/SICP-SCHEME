171efdacfbfb9290a0b2d581e740187a6bc5b5b0
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-02-18 15:54:04 -0500

    WIP

--- hw1-3.scm
+++ hw1-3.scm
@@ -1,50 +1,43 @@
+; there are exactly four cases
+; 1. they all have unique values
+; 2. they all have the same value
+; 3. there is one greatest value
+; 4. there is one least value
+; by finding out how many of the three are larger than at least one other
+; we can easily differentiate between these cases
+; if there are two larger than at least one other then those two are the largest
+; 
+
 (define (sumOfSquares x y)
   (+ (* x x) (* y y))
 )
 
-(define (func x y z)
-  (define sorted (recursiveInsertionSort (list x y z)))
-  (display (first sorted))
-  (display (second sorted))
-  (sumOfSquares (first sorted) (second sorted))
-)
-
-;;; this procedure accounts for all cases,
-;;; there are exactly four cases
-;;; 1. they all have unique values
-;;; 2. they all have the same value
-;;; 3. there is one greatest value
-;;; 4. there is one least value
-;;; finding the least will cover three of these cases 1, 2, 4.
-;;; in 1 and 4 exclude the least, in 2 use any two values
-
-; this works but is crazy inneficient
-(define (recursiveInsertionSort input)
+(define (biggestTwo x y z)
+  (define biggest (append
+    (biggerThanOne x y z)
+    (biggerThanOne y x z)
+    (biggerThanOne z y x)))
   (cond
-    ((= (length+ input) 1)
-      input)
-    ((> (first input) (first (recursiveInsertionSort (cdr input))))
-      (append (list (first input)) (recursiveInsertionSort (cdr input))))
-    (else
-      (append (recursiveInsertionSort (cdr input)) (list (first input))))
+    ((= (length+ biggest) 2) biggest)
+    ((= (length+ biggest) 0) (list x y))
+    ((= (length+ 1))
+      (if (= x y)
+        (list x z)
+        (list x y)))
   )
 )
 
-(func 2 15 4)
+(define (biggerThanOne x other1 other2)
+  (if (or (> x other1) (> x other2))
+    (list x)
+    (list))
+)
 
-; (define (whichLeast x y z)
-;   (cond
-;     ((isLeast? x y z) x)
-;     ((isLeast? y x z) y)
-;     ((isLeast? z x y) z)
-;     (else #f)
-;   )
-; )
+(define (sumOfSquaresOfBiggestTwo x y z)
+  (define big2 (biggestTwo x y z))
+  (sumOfSquares (first big2) (second big2))
+)
 
-; (define (isLeast? least? other1 other2)
-;   (and (< least? other1) (< least? other2))
-; )
+(sumOfSquaresOfBiggestTwo 9 7 14)
 
-; (whichLeast 1 4 567)
-
-;;; maybe rewrite this entire thing with recursion?
+;output is 277 as it should be
