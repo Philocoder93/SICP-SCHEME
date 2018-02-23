5d7ea29348a4acbe25ca99809f4cec332435e0eb
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-02-23 00:47:33 -0500

    WIP

--- hw1-12.scm
+++ hw1-12.scm
@@ -1,14 +1,15 @@
 ; file will recursively traverse pascals triangle and return as
 ;many rows of it in a list as you specify in the counter variable
 
-(define (recursivePascal counter pasc)
-  (if (= counter 1)
-    '(pasc)
-    (append!
-      pasc
-      (recursivePascal(
-        (- counter 1)
-        (nextRow pasc '()))))))
+(define (recursivePascal counter inList)
+  )
+
+
+
+
+
+ (if (> counter 1)
+   ((append! pasc (recursivePascal (- counter 1) (nextRow pasc '()))))))
 
 
 
@@ -17,10 +18,10 @@
 
 
 
-(define (nth ind list)
-  (if (= ind 0)
-    (first list)
-    (nth (- ind 1) (list-tail list 1))))
+;(define (nth ind list)
+;  (if (= ind 0)
+;    (first list)
+;    (nth (- ind 1) (list-tail list 1))))
 
 (define (nextRow inlist outlist)
   (if (= (length+ inlist) 1)
@@ -31,6 +32,8 @@
         outlist
         (list (+ (first inlist) (second inlist)))))))
 
-(nextRow '(1 3 3 1) '())
+(nextRow '(1 5 10 10 5 1) '())
 
-(recursivePascal 5 '(1))
+;(nextRow '(1 4 6 4 1) '())
+
+;(recursivePascal 2 '(1))
