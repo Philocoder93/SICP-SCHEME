7e3229cb43ee75ee4e870a04f1aaf49a10656648
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-02-25 12:03:57 -0500

    added a comment to euclidsAlgorithm

--- euclidsAlgorithm.scm
+++ euclidsAlgorithm.scm
@@ -12,3 +12,8 @@
 (euclidsAlgorithm 123567 535656)
 
 ; answer outputs as 3
+
+; after looking at how they implemented this algorithm in the book
+; i realize that the third condition is unnecesary,
+; all you need to do is ensure that the initial A is greater than the initial
+; B
