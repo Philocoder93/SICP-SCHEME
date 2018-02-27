836f371014d3a7d70f39166522186537cbfeea78
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-02-27 16:43:20 -0500

    wrapped up hw1-23.scm

--- hw1-23.scm
+++ hw1-23.scm
@@ -3,9 +3,19 @@
 
 (define (smallestDivisorIter N D)
   (cond
+    ((> (square D) N) N)
     ((divisor? N D) D)
     ((= D 2) (smallestDivisorIter N 3))
     (else (smallestDivisorIter N (+ D 2)))))
 
 (define (smallestDivisor N)
   (smallestDivisorIter N 2))
+
+
+(smallestDivisor 357)
+
+; 3, as it should be
+
+(smallestDivisor 917)
+
+; 7 as it should be
