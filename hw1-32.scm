e5753b15618dda6203a2a63ab11bced2ce7ba508
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-02-28 18:12:00 -0500

    wrapped up hw1-32.scm

--- hw1-32.scm
+++ hw1-32.scm
@@ -23,9 +23,16 @@
   (+ a b))
 
 
-(reduce 2 6 check1 0 fcn1 step1 acumFunc1)
+(reduce 2 9 check1 0 fcn1 step1 acumFunc1)
 
 ; this should output the numbers 1 to 10 with 2 added to them and them multiplied together
 
 
 ; @TODO write the iterative version of this procedure
+
+(define (reduceIter a b check acum fcn step acumFunc)
+  (if (check a b)
+    acum
+    (reduceIter (step a) b check (acumFunc (fcn a) acum) fcn step acumFunc)))
+
+(reduceIter 2 9 check1 0 fcn1 step1 acumFunc1)
