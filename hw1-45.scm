f4d18fc2f2f1bfa232abf78d32727a978917b729
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-04 15:19:38 -0500

    WIP, setting this one aside for now

--- hw1-45.scm
+++ hw1-45.scm
@@ -16,10 +16,12 @@
 
 
 
-(define (NthCompose fb fa n)
+(define (NthCompose fa fb n)
   (if (<= n 0)
     fa
-    (NthCompose fb (compose fb fa) (- N 1))))
+    (NthCompose fb (compose fa fb) (- N 1))))
+
+; fun fact, this procedure will be very helpfull in calculating the thing that I am interested in
 
 (define (avg-damp f)
   (display '2) (newline )
@@ -28,8 +30,26 @@
 (define (fourthRoot0f12 y)
   (/ 12 (* y y y)))
 
+(define (getNthrootFunc Y Nth)
+  (if (= Nth 0)
+        1
+        (* Y (getNthrootFunc Y (- Nth 1)))))
+
+
+
 (define (fRO12DoubleDamped)
   (display '1) (newline )
   (compose (avg-damp fourthRoot0f12) avg-damp) )
 
 (fixed_point (fRO12DoubleDamped ) 1)
+
+
+(define (Nthroot Num Nth)
+  (fixed_point
+              (NthCompose
+                        (lambda (y) (/ Num (getNthrootFunc y Nth)))
+                        avg-damp
+                        (- Nth 1))
+              1))
+
+(Nthroot 5 5)
