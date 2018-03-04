9bb3c972a92b1e1a967fd91dec755ee6f3137aa2
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-03 20:56:59 -0500

    WIP

---
+++ hw1-46.scm
@@ -0,0 +1,19 @@
+; this is the iterative improvement procedure
+; this article: https://stackoverflow.com/questions/7719004/in-scheme-how-do-you-use-lambda-to-create-a-recursive-function
+; helped a lot with this question
+
+
+(lamda (a)
+  (if (ge a)
+    a
+    (improveIter f ge (f a))))
+
+
+
+
+(define (improve f ge)
+  ((lambda (x) (x x f ge 1)) (lambda (improveGen f ge a)
+    (lambda (a)
+      (if (ge a)
+        a
+        (improveGen improveGen f ge (f a)))))))
