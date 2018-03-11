a045402375f34fb20109f663d65ca76c4277da12
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-11 08:20:23 -0400

    wrote 2-15

---
+++ hw2-15.scm
@@ -0,0 +1,4 @@
+; not insofar as I can see, in my experience it looks like method1 has tighter
+; error bounds, as far as I can see it doesn't really matter how many times
+; the variable appears, it matters how many instances of interval division and subtraction
+; occur on the way there
