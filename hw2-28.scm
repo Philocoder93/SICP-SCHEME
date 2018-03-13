842e160d1b27830b95a9a2537c285835615632d3
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-12 20:43:50 -0400

    wrapped up 2-28

--- hw2-28.scm
+++ hw2-28.scm
@@ -1,20 +1,14 @@
 (define (get-fringe lst)
-(newline ) (display lst)
-  (let (
-        (endOfList? (lambda (lst)
-          (newline )(display 'fireleaf)
-          (if (list? lst)
-            (null? (cdr lst))
-            #f))))
-    (cond
-      ((endOfList? lst)
-        (newline )(display 'first-path)
-        (list (car lst)))
-      ((list? lst)
-        (newline )(display 'second-path)
-        (append (get-fringe (car lst)) (get-fringe (cdr lst))))
-      (else
+  (cond
+    ((null? lst) '())
+    ((list? lst) (append (get-fringe (car lst)) (get-fringe (cdr lst))))
+    (else (list lst))))
 
-        (list lst)))))
+; there's a longer and slower way to do this where at each node you check to see
+; if the cdr of the list you are at is null and if so you get-fringe just the car
+; but i forgot that the accumulator op is actually append and you can feed null
+; to append and it will just end up with the non null list
 
-(get-fringe (list 2 (list 3 (list 3 7 9) 5) 2 3 4) )
+(get-fringe (list 2 (list 3 (list 3 7 9) 1 2) 2 3 4))
+
+; returns (2 3 3 7 9 1 2 2 3 4)
