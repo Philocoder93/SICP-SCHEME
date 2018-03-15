44b6d3bbecd157fc8a6a5c3475affca7107382ad
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-14 20:12:39 -0400

    wrote 2-35

---
+++ hw2-35.scm
@@ -0,0 +1,17 @@
+(define (accum oper init lst)
+   (if (null? lst)
+       init
+       (oper (car lst)
+           (accum oper init (cdr lst)))))
+
+(define (get-fringe lst)
+  (cond
+    ((null? lst) '())
+    ((list? lst) (append (get-fringe (car lst)) (get-fringe (cdr lst))))
+    (else (list lst))))
+
+(define (leaf-count t)
+  (accum (lambda (x y) (+ y 1)) 0 (map get-fringe (list t))))
+
+
+(define (leaf-count (list 4 5 5 (list 1 2 3 (list 3 4 5)))))
