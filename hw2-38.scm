88b158e98ae68024877c7f3bd9ba1a2914e0271e
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-15 21:43:09 -0400

    wrote hw2-38

---
+++ hw2-38.scm
@@ -0,0 +1,13 @@
+; 3/2
+
+; 1/6
+
+; (1 (2 (3 nil)))
+
+; (((nil 1) 2) 3)
+
+; the op should be both commutative and associative (i.e. you should be able to
+; mess around with the order in which the elements are presented *and* evaluated)
+; this is probably only achievable with an operation and seed value combo that
+; allows the seed value to entirely dissapear into the first element, for instance
+; 1 and multiplication or zero and addition
