798edaab76172fd373bede9c65a95f69fc3f26bc
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-21 18:25:19 -0400

    made corrections to 2-50

--- h2-50.scm
+++ h2-50.scm
@@ -8,21 +8,21 @@
 (define (flip-painter-180deg painter)
   (shift-painter painter
                  (cons-vect 1.0 1.0)
-                 (cons-vect 1.0 0.0)
-                 (cons-vect 0.0 1.0)))
+                 (cons-vect 0.0 1.0)
+                 (cons-vect 1.0 0.0)))
 
 ; remember that rotating something 90 degrees clockwise is the same as rotating it 270 degrees
 ; counterclockwise, so this is the answer to the 270 degrees question
 (define (flip-painter-90deg painter)
   (shift-painter painter
                  (cons-vect 0.0 1.0)
-                 (cons-vect 1.0 1.0)
-                 (cons-vect 0.0 0.0)))
+                 (cons-vect 0.0 0.0)
+                 (cons-vect 1.0 1.0)))
 
 ; this one simply produces a left-right mirror image of the original
 ; painter
 (define (flip-painter-horiz painter)
   (shift-painter painter
                  (cons-vect 1.0 0.0)
-                 (cons-vect 1.0 1.0)
-                 (cons-vect 0.0 0.0)))
+                 (cons-vect 0.0 0.0)
+                 (cons-vect 1.0 1.0)))
