888a49fce24285167bce097607ffd217c0f0e113
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-21 21:55:12 -0400

    wrote 2-53

---
+++ hw2-53.scm
@@ -0,0 +1,14 @@
+; (a b c)
+
+; ((george))
+
+; this is weird, i totally expected this
+; to throw y1 not applicable, i guess quote
+; can be used to create multilevel data
+; ((y1 y2))
+
+; (y1 y2)
+
+; #f
+
+; (red shoes blue socks)
