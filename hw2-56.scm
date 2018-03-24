f2d3e1199a3fd7cb1e879cc497a6a16525dbf5b3
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-24 13:38:43 -0400

    skipping 2-56 to 2-58 for now

---
+++ hw2-56.scm
@@ -0,0 +1,3 @@
+; I am going to skip 2-56-58 for now because, simply put, I do not understand the math
+; yet, I will return and do these problems when I understand the math, for now though
+; I think I understand the programming principles quite well
