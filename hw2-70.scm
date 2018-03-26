f1386931112cc76089034a51115b257976a43c70
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-25 22:14:28 -0400

    wrote 2-70

---
+++ hw2-70.scm
@@ -0,0 +1,7 @@
+; 84 bits were needed to encode
+; this
+
+; the smallest number required if you used
+; a fixed length code is 108, 36 symbols to be encoded,
+; 8 types of symbols, 2 to the 3rd is 8 so three bits per letter
+; 3 times 36 = 108
