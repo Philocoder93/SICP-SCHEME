3dfceddefd5ccb1cda651dd1cbd9189e1b13fad6
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-27 19:26:18 -0400

    write 2-71 2-72

---
+++ hw2-71.scm
@@ -0,0 +1,21 @@
+; 1 2 4 8 16
+
+; bits required to encode the most common symbol is one obviously
+; because the very first fork in this tree will have the most common symbol
+; as one of it's branches and the rest of the tree on the other
+
+; for the least will require n - 1 symbols to encode obviously
+
+; all of this follows straightforwardly from the fact that in this tree each level of the tree will have precisely one branch on it
+; except the last one which will have two,
+; moreover, each branch will have a character of lower frequency than the one that was on the previous line        
+
+
+
+
+
+
+
+
+
+ 
---
+++ hw2-72.scm
@@ -0,0 +1,12 @@
+; order of growth of the most frequent: O(N)
+; the most frequent will run in linear time becuase it will check the symbols present in both the left
+; and right branches at the top level, it will then immediately find that the most frequent symbol is directly on the top
+; branch and then proceed there and find it 
+
+
+; the order of growth of the least frequent will be: O( n + n-1 + n-2 ... 2)  
+
+
+
+
+
