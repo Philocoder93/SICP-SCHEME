e54cb6f0d4df46a3c466d42597a472a67504dbed
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-04-04 22:35:01 -0400

    WIP

---
+++ hw2-80.scm
@@ -0,0 +1,14 @@
+(define (install-zero-equals)
+	(define (rational-zero-equals x)
+		(= x 0))
+
+	(define (complex-zero-equals x)
+		( 
+
+	(define (scheme-number-zero-equals ) 
+
+	(put '=zero? '(scheme-number scheme-number) 
+
+	(put '=zero? '(complex complex) 
+
+	(put '=zero? '(rational rational)
