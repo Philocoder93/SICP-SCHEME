e9a83c028c536fa8cd6a6fa5e6f61fc6ddb5fd1f
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-04-25 19:18:09 -0400

    WIP

---
+++ hw3-17.scm
@@ -0,0 +1,45 @@
+(define (gen-memoized-count-pairs)
+
+	(define completed-list '())
+
+	(define (completed? x)
+		(define (completed-iter x x-list)
+			(newline )
+			(display 'fire)
+			(display x-list)
+			(cond
+				((null? x-list) #f)
+				((eq? x (first x-list)) #t)
+				(else (completed-iter x (cdr x-list)))))
+		(completed-iter x completed-list))
+
+	(define (add-to-completed x)
+		(display 'this-is-it)
+		(display x)
+		(if (null? completed-list)
+			(set! completed-list (cons x '()))
+			(set-cdr! (last-pair completed-list) (cons x '()))))
+
+	(define (memoized-count-pairs x)
+		(cond
+			((not (pair? x)) (display 'crap))
+			((completed? x) (display 'shit))
+			(else (begin
+				(add-to-completed x)
+				(+
+					(memoized-count-pairs (car x))
+					(memoized-count-pairs (cdr x))
+					1)))))
+
+	memoized-count-pairs
+)
+
+
+(define count-pairs (gen-memoized-count-pairs ))
+
+count-pairs
+
+(define test-example (cons (cons 'a 'b) (cons 'c 'd)))
+
+(count-pairs test-example)
+
