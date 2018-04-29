e36979241b92e66a32a4edae37630448d882c602
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-04-29 09:37:30 -0400

    write 3-18

--- hw3-18.scm
+++ hw3-18.scm
@@ -1,64 +1,3 @@
-(define (nth-cdr n lst)
-	(if (= n 0)
-		lst
-		(cdr (nth-cdr (- n 1) lst))))
-
-(define test-list (list 'A 'B 'C 'D 'E))
-(set-cdr! (last-pair test-list) test-list)
-
-
-(car (nth-cdr 0 test-list))
-
-(car (nth-cdr 2 test-list))
-
-(car (nth-cdr 4 test-list))
-
-(eq? (nth-cdr 0 test-list) (nth-cdr 5 test-list))
-
-
-(error "stop here")
-(define (ultra-cycle-sniffer hlst)
-	(define (cycle-sniffer lst)
-		(define initial lst)
-		(display 'fireinner)
-		(newline )
-		(define (cycle-catcher-iter cur)
-			(cond
-				((not (pair? cur)) #f)
-				((null? (cdr cur)) #f)
-				((eq? initial (cdr cur)) #t)
-				(else (cycle-catcher-iter (cdr cur)))))
-		(cycle-catcher-iter lst))
-(display 'fire)
-(newline )
-	(cond
-		((not (pair? hlst)) #f)
-		((cycle-sniffer hlst) #t)
-		(else (ultra-cycle-sniffer (cdr hlst)))))
-
-
-(define cycle (list 'A 'B 'C 'D 'E))
-
-(ultra-cycle-sniffer cycle)
-
-(set-cdr! (last-pair cycle) cycle)
-
-(ultra-cycle-sniffer cycle)
-
-(define new-cycle (list 'x 'y))
-
-(set-cdr! (last-pair new-cycle) cycle)
-
-(ultra-cycle-sniffer new-cycle)
-
-
-; so the solution needs to go like this, the top level procedure needs to go down the list and point to what each pair points to in turn
-; (so first it points to what the first pairs cdr points to, then it points to what the second pairs cdr points to, then it points to what the
-; third pairs cdr points to and so on, then it needs to check that value against all the preceding car values of the previous links, if it finds
-; no match then proceed to the next cdr, the only exit condition is that you run out of cdrs or you find a match (in which case indicate that there are
-; cycles).
-
-
 (define (cycle-sniffer lst)
 	(define initial lst)
 	(define (cycle-catcher-iter cur)
@@ -70,33 +9,49 @@
 	(cycle-catcher-iter lst))
 ; this ^^^ algorithm works if we assume that the cycle is going to be end to end
 ; that is, if the last pair in the linked list will have the entire original list
-; as it's cdr, if it has a subset of the linked list then you need a modified version
-; of this
+; as it's cdr, if it has a subset of the linked list then you need a much more elaborate
+; procedure
 
+(define (cycle-sniffer arg-lst)
 
+	;this method is an nth cdr method
+	(define (nth-cdr n lst)
+		(display 'a)
+		(if (= n 0)
+			lst
+			(cdr (nth-cdr (- n 1) lst))))
 
+	;this method is a terminator function
+	(define (done x y lst)
+		(display 'b)
+		(null? (nth-cdr y lst)))
 
+	;this method is a loop checker function
+	(define (loop x y lst)
+		(display 'c)
+		(eq? (nth-cdr x lst) (nth-cdr y lst)))
 
+	;this method is a double loop method
+	(define (double-loop x y terminate-func check-func lst)
+		(display 'd)
+		(cond
+			((> x y) (error "loop got out of whack"))
+			((= x y) (double-loop 0 (+ y 1) done loop arg-lst))
+			((terminate-func x y lst) #f)
+			((check-func x y lst) #t)
+			(else (double-loop (+ x 1) y done loop arg-lst))))
 
+	(double-loop 0 1 done loop arg-lst))
 
+(define cycle (list 'A 'B 'C 'D 'E))
+(cycle-sniffer cycle)
+; this returns false
 
+(set-cdr! (last-pair cycle) cycle)
+(cycle-sniffer cycle)
+; this returns true
 
-
-
-
-
-
-
-
-(define (top-level func lst)
-	(
-
-
-
-
-
-
-
-
-
-
+(define new-cycle (list 7 8 9))
+(set-cdr! (last-pair new-cycle) cycle)
+(cycle-sniffer new-cycle)
+; this return true
