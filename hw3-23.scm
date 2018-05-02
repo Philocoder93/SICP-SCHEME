3ab81058d6018ce6ad41c7cb787aa38b5e6f70e0
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-05-02 19:48:33 -0400

    WIP

--- hw3-23.scm
+++ hw3-23.scm
@@ -1,26 +1,70 @@
-(define (make-dek ) (cons '() '()))
+; there needs to be a new paradigm for deqs
+; basically they need to be constructed out of something besides
+; pairs, i have chosen to build them out of links
 
-(define (front-point dek)  (car x))
 
-(define (back-point dek) (cdr x))
+(define (make-lnk x) (list '() x '()))
 
-(define (set-front! dek x) (set-car! dek x))
+(define (front-point-lnk lnk)  (first lnk))
 
-(define (set-back! dek x) (set-cdr! dek x))
+(define (get-val-lnk lnk) (second lnk))
+
+(define (back-point-lnk lnk) (third lnk))
+
+(define (set-front-lnk lnk x) (set-car! lnk x))
+
+(define (set-back-lnk lnk x) (set-car! (cddr lnk) x))
 
 ;abstraction layer
 
+; the book reccomends using more basic operations, i don't think you actually need that many core operations
+; you can do a lot of this stuff much more simply with less core operations
+
+(define (make-dek )
+	(let ((nu-lnk (make-lnk '())))
+		(cons nu-lnk nu-lnk)))
+
+(define (empty-dek dek)
+	(eq? (front-dek dek) '()))
+
+(define (front-dek dek)
+	(car (get-val-lnk dek)))
+
+(define (rear-dek dek)
+	(cdr (get-val-lnk dek)))
+
+(define (insert-front-dek dek x)
+	(let ((nu-lnk (make-lnk x)))
+		(set-front-lnk (car dek) nu-lnk)
+		(set-car! dek nu-lnk)))
+
+(define (insert-rear-dek dek x)
+	(let ((nu-lnk (make-lnk x)))
+		(set-back-lnk (cdr dek) nu-lnk)
+		(set-cdr! dek nu-lnk)))
+
+(define (i
+
+
+
+
+
+
+;abstraction layer
+
+(define (make-queue
+
 (define (empty-dek dek) (null? (front-point dek)))
 
-(define (front-dek x)
-	(if (empty-dek x)
+(define (front-dek dek)
+	(if (empty-dek dek)
 		(error "empty")
-		(car (front-point x))))
+		(car (front-point dek))))
 
-(define (back-dek x)
-	(if (empty-dek x)
+(define (back-dek dek)
+	(if (empty-dek dek)
 		(error "empty")
-		(car (back-point x))))
+		(car (back-point dek))))
 
 (define (front-insert-dek! dek x)
 	(cond
@@ -28,18 +72,22 @@
 			(set-front! dek x)
 			(set-back! dek x))
 		(else
-			(set-front! dek )
-			))
+			(set-front! dek (cons x (front-point x)))
+			)))
 
-(define (back-insert-dek! x)
+(define (back-insert-dek! dek x)
 	(cond
 		((empty-dek dek)
 			(set-front! dek x)
 			(set-back! dek x))
 		(else
-			()
-			)))
+			(let ((new (cons x '())))
+				(set-cdr! (back-point x) new)
+				(set-back! dek new)))))
+	
 
-(define (front-delete-dek! x))
+(define (front-delete-dek! dek)
+	(set-front! dek (cdr (front-point dek))))
 
-(define (back-delete-dek! x))
+(define (back-delete-dek! dek)
+	(set-front! dek ))
