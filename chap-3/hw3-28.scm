7aec6302bef24c3fc80dcc44239a76bec2207fa0
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-05-06 08:36:10 -0400

    update 3-28 and write 3-29

--- chap-3/hw3-28.scm
+++ chap-3/hw3-28.scm
@@ -1,7 +1,7 @@
 (define (or-gate o1 o2 ouput)
 	(define (or-proc )
 		(let ((new-value
-			(or (get-signal o1) (get-signal o2))))
+			(logical-or (get-signal o1) (get-signal o2))))
 			(after-delay or-gate-delay
 				(lambda () (set-signal! output new-value)))))
 	(add-action! o1 or-proc)
