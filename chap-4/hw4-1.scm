84547c9f20dc5ff3c04eb1ff8645ca8abebb7b73
iduarte <isaac.bn.duarte@gmail.com>
2018-05-21 20:22:56 -0400

    write 4-1

--- chap-4/hw4-1.scm
+++ chap-4/hw4-1.scm
@@ -1,1 +1,17 @@
-; this is the first exercise in this
+(define (list_val_left_to_right expr env)
+	(if (no_oper expr)
+		'()
+		(let (
+			(first-arg (eval (frst_oper expr) env)))
+			(cons
+				first-arg
+				(list_vals (rest_oper expr) env) ))))
+
+(define (list_val_right_to_left expr env)
+	(if (no_oper expr)
+		'()
+		(let (
+			(second-arg (list_vals (rest_oper expr) env)))
+			(cons
+				(eval (frst_oper expr) env)
+				second-arg))))
