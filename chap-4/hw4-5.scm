2e8d61b48d813b2c58e5f388b9eb53166bf71acf
iduarte <isaac.bn.duarte@gmail.com>
2018-05-27 11:27:00 -0400

    wrote 4-5

--- chap-4/hw4-5.scm
+++ chap-4/hw4-5.scm
@@ -1,17 +1,35 @@
 (define (arrow-clause expr)
 	(tagged-list? (cond-actions expr) '=>))
 
-(define (arrow-clause-func expr)
-	(cdr (cond-actions expr)))
+(define (get-arrow-clause-pred expr)
+	(car expr))
+
+(define (get-arrow-clause-body expr)
+	(cddr expr))
+
+(define (arrow-clause-apply expr pred)
+	(cons expr pred))
+
+(define (one-arg-fnc input)
+	; returns true if the input is a one arg function
+	; not sure how to check this, i think if it's possible
+	; then it would have to be a special keyword exposed by
+	; the language itself
+		)
 
 (define (clause-expand clauses)
-	; lines 2 through 5 from the book
+; lines 2 through 5 from the book
 	(cond (
 		((else-clause? frst) (if (null? rst)
 			(seq->expr (cond-act frst))
 			(error "else should always be last in the clause order"))
-		((arrow-clause rst) (if (one-arg-fnc (arrow-clause-func expr))
-			(if (eval (cond-predicate))
+		((arrow-clause rst) (if (one-arg-fnc (get-arrow-clause-body frst))
+			(make-if
+				(get-arrow-clause-pred frst)
+				(arrow-clause-apply
+					(get-arrow-clause-body frst)
+					(get-arrow-clause-pred frst)
+					(clause-expand rst)))
 			(error "must be one arg func")))
-		(else )))))
-
+; the rest of this crap
+		))))
