b95fe80041b2c4c6312cb05120a64256ec041d93
iduarte <isaac.bn.duarte@gmail.com>
2018-05-30 20:37:00 -0400

    wrote 4-18

---
+++ chap-4/hw4-18.scm
@@ -0,0 +1,5 @@
+; no this procedure will not work
+; this is because when the second let is evaluated define dy
+; will will use y at a time when using y will throw an error
+; the normal version would fail too if it wasn't for the delay
+; in the define y
