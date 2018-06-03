01dff53026df31894f0f26d2b19659cec5f71f77
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-06-03 12:04:00 -0400

    write 4-23

---
+++ chap-4/hw4-23.scm
@@ -0,0 +1,11 @@
+; in the case of one expression
+; the first procedure just is the correct procedure call
+; the second one has to do a check on two memory
+; locations and progress through a cond block before
+; calling it
+;
+; in the case of two expressions
+; the first procedure calls both sub procedures in it's body
+; the second one has go through two cond blocks access
+; five memory locations and do a recursive call
+
